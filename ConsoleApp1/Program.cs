4292ce01c325e1770719dabdd77dd490b96752d8
Abby Thompson <abby.thompson@c02tq0bphtd8.myob.myobcorp.net>
2019-07-25 14:59:55 +1200

    katas 1 through 7

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Give me a number.");
+
+            var response = Console.ReadLine();
+            
+            var num = int.Parse(response);
+            
+            Console.WriteLine("Do you want the Sum(+) or the product(x)?");
+            response = Console.ReadLine();
+            
+            var sum = 0;
+
+            if (response == "+")
+            {
+                for (var i = 1; i < num + 1; i++)
+                {
+                    sum += i;
+                }
+            } else if (response == "x")
+            {
+                sum = 1;
+                for (var i = 1; i < num + 1; i++)
+                {
+                    sum *= i;
+                }
+            }
+            else
+            {
+                Console.WriteLine("wrong choice");
+                return;
+                
+            }
+            
+            Console.WriteLine(sum);
+
+
+            //if (num % 5 != 0 || num % 3 != 0) return;
+
+//            if (name == null) return;
+// 
+//            
+//            if (String.Compare(name, "bob", StringComparison.OrdinalIgnoreCase) == 0 || String.Compare(name, "alice", StringComparison.OrdinalIgnoreCase) == 0)
+//            {
+//                Console.WriteLine($"Hi, {name}");
+//            }
+        }
+    }
+}
